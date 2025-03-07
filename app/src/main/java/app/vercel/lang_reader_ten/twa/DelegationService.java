3380a7b16ac677662250165fb8dbe75e1fba6c20
fabio <fabiod898@gmail.com>
2025-03-06 22:45:30 -0500

    first commit

---
+++ app/src/main/java/app/vercel/lang_reader_ten/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.lang_reader_ten.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
